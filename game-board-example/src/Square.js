b6137868523c1ef46fe5a54f5cc10805f40c9298
ViktorDimitrov1989 <viktor.dimtirov.1989@gmail.com>
2017-11-01 10:41:15 +0200

    official reactJS exercise

---
+++ game-board-example/src/Square.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+class Square extends Component {
+
+    render() {
+        return (
+            <button className="square" onClick={() => { this.props.onClick()}}>
+                {this.props.value}
+            </button>
+        );
+    }
+}
+
+export default Square;
+
