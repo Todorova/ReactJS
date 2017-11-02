5ddced9a7e1cd883358af0a0ff845f6a2d8e9460
Viktor <viktor.dimitrov.1989@gmail.com>
2017-11-03 00:35:17 +0200

    main functionalities for the events-and-forms-exercises created

--- events-and-forms-exercises/src/App.js
+++ events-and-forms-exercises/src/App.js
@@ -1,8 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import './App.css'
 
-import SingUpForm from './components/form/SingUpForm'
-import LoginForm from './components/form/LoginForm'
+import SingUpForm from './components/form/SingUpForm';
+import LoginForm from './components/form/LoginForm';
+import CreatePokemonField from './components/form/CreatePokemonForm';
+
+
 class App extends Component {
   constructor() {
     super()
@@ -11,13 +14,36 @@ class App extends Component {
       username: '',
       token: ''
     }
+
+    this.authenticate = (data) => {
+      if (data.success) {
+        this.setState({ token: data.token, username: data.user.name });
+        localStorage.setItem('token', data.token);
+      }
+    }
+
   }
 
+  componentDidMount() {
+    this.setState({ token: localStorage.getItem('token') });
+  }
+
+
   render() {
+    if (this.state.token !== undefined
+      && this.state.token !== 'undefined'
+      && typeof (localStorage.token) !== 'undefined') {
+      return (
+        <div>
+          <CreatePokemonField/>
+        </div>
+      )
+    }
+
     return (
       <div>
         <SingUpForm />
-        <LoginForm />
+        <LoginForm authFunc={this.authenticate} />
       </div>
     )
 
